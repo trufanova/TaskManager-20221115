2e1115cd7cc507c8d24aac6cabee07af093f3ced
Maria Trufanova <5014685@mail.ru>
2022-03-21 19:45:13 +0300

    Add gem slim-rails, create controllers  Web::Boards and Web::Sessions and tests for them"

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,8 +1,17 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :name, :password, :state] do |n|
     "string#{n}"
   end
+
+  sequence :text, aliases: [:description] do |n|
+    "text#{n}"
+  end
+
   sequence :email do |n|
     "email#{n}@email.com"
+  end
+
+  sequence :expired_at do |n|
+    n.days.ago
   end
 end
