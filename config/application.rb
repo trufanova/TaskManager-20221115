3a7fb1358342ba2adf3cbf26d27bb76123d7a8ad
Maria Trufanova <5014685@mail.ru>
2022-03-23 11:34:51 +0300

    add material-design-lite and layout site

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,7 +12,7 @@ module TaskManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.assets.paths << Rails.root.join('node_modules')
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
